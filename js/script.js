a0c4a78304bd98bb897f173467dc4bbdad5152f3
viniciusM98 <vinimed.98@gmail.com>
2019-10-10 14:11:42 -0300

    Add todo tasks with icons, but whitout save

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const listas = document.querySelectorAll('ul')
+const todoCard = document.querySelector('#todo')
+const inputToDo = document.querySelector('#tarefa')
+const addButton = document.querySelector('#addButton')
+//const nextButton = document.querySelector("#nextBt")
+
+/*const save = () => {
+  let listasTransformadas = Array.from(listas)
+    .map(lista => {
+      return {
+        nomeLista: lista.getAttribute('id'),
+        itens: Array.from(lista.children)
+          .map(filho => {
+            const [tarefa] = filho.querySelectorAll('li')
+            return {
+              tarefa: tarefa.textContent
+            }
+          })
+      }
+    })
+
+    localStorage.setItem('lista', JSON.stringify(listasTransformadas))
+}*/
+const transitarDoing = (item) => {
+  
+}
+
+const criarItem = (tarefa) => {
+  const li = document.createElement('li')
+  const btNext = document.createElement('i')
+  const button = document.createElement('button')
+
+  btNext.setAttribute('class', 'fas fa-chevron-circle-right')
+
+  button.appendChild(btNext)
+
+  button.onclick = () => transitarDoing(li)
+
+  inputToDo.textContent = tarefa
+
+  li.append(tarefa, button)
+
+  todoCard.appendChild(li)
+
+  //tarefa.appendChild(li)
+
+  // <li>Revisar HTML e CSS <button><i class="fas fa-chevron-circle-right"></i></button></li>
+  // <li>Fazer a o Todo App <button><i class="fas fa-chevron-circle-right"></i></button></li>
+  // <li>Postar meu código no GitHub <button><i class="fas fa-chevron-circle-right"></i></button></li>
+  // <li>Ir na aula de JS <button><i class="fas fa-chevron-circle-right"></i></button></li>
+}
+
+const adicionarLista = () => {
+  if (inputToDo.value) {
+    criarItem(inputToDo.value + ' ')
+    //save()
+    inputToDo.value = ''
+  }
+}
+
+addButton.onclick = adicionarLista
