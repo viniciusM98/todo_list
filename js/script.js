59e2613846474bf38aea0dbb3f3cd57cecb12c07
viniciusM98 <vinimed.98@gmail.com>
2019-10-14 10:48:30 -0300

    Adjusts

--- js/script.js
+++ js/script.js
@@ -4,30 +4,6 @@ const doingCard = document.querySelector('#doing')
 const doneCard = document.querySelector('#done')
 const inputToDo = document.querySelector('#tarefa')
 const addButton = document.querySelector('#addButton')
-//const nextButton = document.querySelector("#nextBt")
-
-/*const save = () => {
-  let listasTransformadas = Array.from(listas)
-    .map(lista => {
-      return {
-        nomeLista: lista.getAttribute('id'),
-        itens: Array.from(lista.children)
-          .map(filho => {
-            const [tarefa] = filho.querySelectorAll('li')
-            return {
-              tarefa: tarefa.textContent
-            }
-          })
-      }
-    })
-
-    localStorage.setItem('lista', JSON.stringify(listasTransformadas))
-}*/
-
-/*const transitarDone = (dItem) => {
-  doingCard.removeChild(dItem)
-  doneCard.appendChild(dItem)
-}*/
 
 const excluirItem = (eItem) => {
   doneCard.removeChild(eItem)
@@ -83,19 +59,11 @@ const criarItem = (tarefa) => {
   li.append(tarefa, button)
 
   todoCard.appendChild(li)
-
-  //tarefa.appendChild(li)
-
-  // <li>Revisar HTML e CSS <button><i class="fas fa-chevron-circle-right"></i></button></li>
-  // <li>Fazer a o Todo App <button><i class="fas fa-chevron-circle-right"></i></button></li>
-  // <li>Postar meu código no GitHub <button><i class="fas fa-chevron-circle-right"></i></button></li>
-  // <li>Ir na aula de JS <button><i class="fas fa-chevron-circle-right"></i></button></li>
 }
 
 const adicionarLista = () => {
   if (inputToDo.value) {
     criarItem(inputToDo.value + ' ')
-    //save()
     inputToDo.value = ''
   }
 }
